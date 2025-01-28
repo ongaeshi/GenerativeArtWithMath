21be95fa9e5a5ef01bcfc3f36c45532efc6817b5
ongaeshi <ongaeshi0621@gmail.com>
2025-01-29 03:08:24 +0900

    feat: gcd 関数を抽出

--- Ch1_Euclid/Numeric/Numeric.rb
+++ Ch1_Euclid/Numeric/Numeric.rb
@@ -1,12 +1,18 @@
-# a = 10
-# b = 6
-a = 14803
-b = 12707
-d = b
-while d > 0 do
-  c = a / b
-  d = a % b
-  a = b
-  b = d
+def gcd(a, b)
+  c = b
+  while b > 0 do
+    c = a % b
+    a = b
+    b = c
+  end
+  a
 end
-puts "GCD is #{a}"
+
+puts "GCD is #{gcd(10, 6)}" # 2
+puts "GCD is #{gcd(14803, 12707)}" # 131
+puts "GCD is #{gcd(15, 10)}" # 5
+puts "GCD is #{gcd(54, 24)}" # 6
+puts "GCD is #{gcd(17, 13)}" # 1
+puts "GCD is #{gcd(0, 0)}" # 0
+puts "GCD is #{gcd(7, 0)}" # 7
+puts "GCD is #{gcd(0, 7)}" # 7
